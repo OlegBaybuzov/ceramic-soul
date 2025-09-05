858c58f69cbf19a6c658615e6d924b8090ea4c2b
Oleg <oleg20091301@gmail.com>
2025-09-05 19:27:37 +0500

    final edits

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,33 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import viteImagemin from "vite-plugin-imagemin";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-				catalog: resolve(__dirname, "catalog.html"),
-				blog: resolve(__dirname, "blog.html"),
-				about: resolve(__dirname, "about.html"),
-			},
-		},
-	},
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        catalog: resolve(__dirname, "catalog.html"),
+        blog: resolve(__dirname, "blog.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
+  plugins: [
+    viteImagemin({
+      gifsicle: { optimizationLevel: 3 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.65, 0.8], speed: 4 },
+      svgo: {
+        plugins: [
+          { name: "removeViewBox", active: false },
+          { name: "removeEmptyAttrs", active: true },
+        ],
+      },
+    }),
+  ],
 });
